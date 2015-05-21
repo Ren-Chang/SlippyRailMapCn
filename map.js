33b5456138521334b8e898ad612fe402e6c81fdf
Ren-Chang <renc.whu@gmail.com>
2015-05-21 23:31:03 +0800

    Switch to CartoDB base

--- map.js
+++ map.js
@@ -1,34 +1,41 @@
-var projection = ol.proj.get('EPSG:3857');
-var raster = new ol.layer.Tile({
-	source: new ol.source.OSM()
-});
+var map;
+var raster;
+raster = new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: 'http://s.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        attributions: [new ol.Attribution({ html: ['&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'] })]
+      })
+    })
+/*var parser = new ol.format.WMTSCapabilities();
+//$.ajax('http://t0.tianditu.com/ter_w/wmts?request=GetCapabilities&service=wmts').then(function(response) {
+	var result = parser.read(response);
+	var options = ol.source.WMTS.optionsFromCapabilities(result, 
+		{layer:'ter', matrixSet:'w'});
+	raster = new ol.layer.Tile({
+		source: new ol.source.WMTS(options)
+	});*/
 
-var map = new ol.Map({
-	controls: ol.control.defaults({
-		attributionOptions:({
-			collapsible: false
+	map = new ol.Map({
+	//var map = new ol.Map({
+		controls: ol.control.defaults({
+			attributionOptions:({
+				collapsible: false
+			})
+		}),
+		layers: [raster],
+		target: document.getElementById('map'),
+		view: new ol.View({
+			center: ol.proj.fromLonLat([105, 35]),
+			zoom: 4
 		})
-	}),
-	layers: [raster],
-	target: document.getElementById('map'),
-	view: new ol.View({
-		center: ol.proj.fromLonLat([105, 30]),
-		zoom: 4
-	})
-});
-/*var kml = new ol.layer.Vector({
-	source: new ol.source.Vector({
-		url: './data/bjline.kml',
-		format: new ol.format.KML()
-	})
-});*/
-var xyz = new ol.layer.Tile({
-  source: new ol.source.XYZ({
-    attributions: [new ol.Attribution({
-		html: 'Railway tiles rendered by Luke, hosted by GitHub.'
-	})],
-    url: './testtile/{z}/{x}/{y}.png'
-  })
-});
-//map.addLayer(kml);
-map.addLayer(xyz);
+	});
+	var xyz = new ol.layer.Tile({
+	  source: new ol.source.XYZ({
+		attributions: [new ol.Attribution({
+			html: '<a href="http://github.com/Ren-Chang">Ren Chang</a>'
+		})],
+		url: './tiles/{z}/{x}/{y}.png'
+	  })
+	});
+	map.addLayer(xyz);
+//});
