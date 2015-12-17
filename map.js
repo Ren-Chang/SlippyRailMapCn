916237a7c803871ddb09b35da92d4007140ba71b
Ren-Chang <renc.whu@gmail.com>
2015-12-17 18:32:33 +0800

    Add official boundary
    add MapWorld WMS boundary layer, however failed to solve the conflicting
    color of the boundaries

--- map.js
+++ map.js
@@ -12,7 +12,7 @@ terrain = new ol.layer.Tile({
 		url: 'http://t{0-1}.tianditu.com/ter_w/wmts?' + 
 			'service=WMTS&request=GetTile&version=1.0.0&layer=ter&style=default&format=tiles&' + 
 			'tileMatrixSet=w&tileMatrix={z}&tileRow={y}&tileCol={x}',
-		attributions: [new ol.Attribution({ html: '&copy; <a href="http://www.tianditu.com/help/contract/contract.html">MapWorld</a>'})]
+		attributions: [new ol.Attribution({ html: '&copy; <a href="http://map.tianditu.com/help/contract/contract.html">MapWorld</a>'})]
 	}),
 	visible: false, type: 'base', title: 'MapWorld terrain'
 });
@@ -21,7 +21,7 @@ imagery = new ol.layer.Tile({
 		url: 'http://t{0-1}.tianditu.com/img_w/wmts?' + 
 			'service=WMTS&request=GetTile&version=1.0.0&layer=img&style=default&format=tiles&' + 
 			'tileMatrixSet=w&tileMatrix={z}&tileRow={y}&tileCol={x}',
-		attributions: [new ol.Attribution({ html: '&copy; <a href="http://www.tianditu.com/help/contract/contract.html">MapWorld</a>'})]
+		attributions: [new ol.Attribution({ html: '&copy; <a href="http://map.tianditu.com/help/contract/contract.html">MapWorld</a>'})]
 	}),
 	visible: false, type: 'base', title: 'MapWorld imagery'
 });
@@ -35,13 +35,24 @@ rail = new ol.layer.Tile({
 	title: 'China railway',
 	extent: ol.proj.transformExtent([75.9659042903028,18.2916806610885,134.51125735793,53.0040163201993],'EPSG:4326','EPSG:3857')
 });
+var boundary = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		attributions: [new ol.Attribution({
+			html: '&copy; <a href="http://map.tianditu.com/help/contract/contract.html">MapWorld</a>'
+		})],
+		url: 'http://zhfw.tianditu.com/gisserver/ZHFW/wms',
+		params: {'LAYERS':'030100','FORMAT':'image/png','TRANSPARENT':'true','TILED':true}
+	}),
+	title: 'China boundary', crossOrigin: null,
+	extent: ol.proj.transformExtent([73,1,135,55],'EPSG:4326','EPSG:3857')
+});
 var base = new ol.layer.Group({
 	title: 'Base maps',
 	layers: [cartodb, terrain, imagery]
 });
 var overlay = new ol.layer.Group({
 	title: 'Overlays',
-	layers: [rail]
+	layers: [rail, boundary]
 });
 
 map = new ol.Map({
